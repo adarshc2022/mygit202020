67e229b6aff9262e193310e918a856225d27d6cd
adarshc2022 <110046953+adarshc2022@users.noreply.github.com>
2022-07-26 20:45:46 +0530

    add files

---
+++ precache-manifest.659ab2d49e9ae884f0969fc81ae7bc52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d88c92bcb50eec574e33",
+    "url": "/static/css/main.f2d6d4b2.chunk.css"
+  },
+  {
+    "revision": "d88c92bcb50eec574e33",
+    "url": "/static/js/main.d88c92bc.chunk.js"
+  },
+  {
+    "revision": "f1b383ba3a9a7dbb0238",
+    "url": "/static/js/1.f1b383ba.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a06908a2149c8b58d5df5108bb3776f",
+    "url": "/index.html"
+  }
+];
